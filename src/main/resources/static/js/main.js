13c527b3cdf0ef2be9d4e839e9c775b3c6efc4e6
fbdev1 <syrkinvictor@gmail.com>
2021-05-12 21:02:55 +0500

    NewUser button

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,18 +2,28 @@
  *
  */
 $(document).ready(function () {
-    $('.table .eBtn').on('click', function (event) {
+    $('.nBtn, .table .eBtn').on('click', function (event) {
         event.preventDefault();
-        let href = $(this).attr('href');
-        $.get(href, function (user){
-            $('.myForm #id').val(user.id);
-            $('.myForm #name').val(user.name);
-            $('.myForm #surname').val(user.surname);
-            $('.myForm #email').val(user.email);
+        var href = $(this).attr('href');
+        var text = $(this).text();
+        if(text=='Edit')
+        {
+            $.get(href, function (user) {
+                $('.myForm #id').val(user.id);
+                $('.myForm #name').val(user.name);
+                $('.myForm #surname').val(user.surname);
+                $('.myForm #email').val(user.email);
+                // $('.myForm #role').val(user.role);
+            });
+            $('.myForm #exampleModal').modal();
+        }else{
+            $('.myForm #id').val('');
+            $('.myForm #name').val('');
+            $('.myForm #surname').val('');
+            $('.myForm #email').val('');
             // $('.myForm #role').val(user.role);
-        })
-        $('.myForm #exampleModal').modal();
-
+            $('.myForm #exampleModal').modal();
+        }
 
 
     });
